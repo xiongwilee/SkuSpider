48f5d7b8335e64b72ebcef7b0f444850e788f458
xiongwilee <xiongwilee@foxmail.com>
2017-09-19 13:16:36 +0800

    upd: 升级Promise迭代器

--- src/skuSpider.js
+++ src/skuSpider.js
@@ -269,6 +269,30 @@ function saveCsv(fileName, content) {
  */
 function promiseGenerator(arr) {
   const data = [];
+
+  // 添加迭代队列，以获取最后一次的promise执行结果
+  arr.push(null);
+
+  return arr.reduce((sum, value, index) => {
+    return sum.then((result) => {
+      // 忽略初始数据，Promise.resolve()的返回数据
+      if (index !== 0) data.push(result);
+
+      // 如果到最后一个节点，则完成迭代，返回总体数据
+      if (value === null) return data;
+
+      return value();
+    })
+  }, Promise.resolve())
+}
+
+/**
+ * 多个promise顺序执行器
+ * @param  {Array} arr  数组，每个元素均为function，每个function均返回Promise
+ * @return {Object}     Pormise
+ */
+/*function promiseGenerator(arr) {
+  const data = [];
   arr.push(null);
 
   return gen(arr)
@@ -292,4 +316,4 @@ function promiseGenerator(arr) {
       return gen(arr.slice(1), next)
     }
   }
-}
+}*/
