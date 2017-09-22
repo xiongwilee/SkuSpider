e73a96bae2a811552c29aebe3d55e1feff4f0965
xiongwilee <xiongwilee@foxmail.com>
2017-09-22 09:53:41 +0800

    upd: 优化迭代标记方法

--- src/skuSpider.js
+++ src/skuSpider.js
@@ -269,17 +269,18 @@ function saveCsv(fileName, content) {
  */
 function promiseGenerator(arr) {
   const data = [];
-
+  const last = Symbol('last');
+  
   // 添加迭代队列，以获取最后一次的promise执行结果
-  arr.push(null);
+  arr.push(last);
 
   return arr.reduce((sum, value, index) => {
     return sum.then((result) => {
-      // 忽略初始数据，Promise.resolve()的返回数据
+      // 忽略初始数据，即Promise.resolve()的返回数据
       if (index !== 0) data.push(result);
 
       // 如果到最后一个节点，则完成迭代，返回总体数据
-      if (value === null) return data;
+      if (value === last) return data;
 
       return value();
     })
