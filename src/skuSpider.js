dfbe92543bdf5190fc051f4795e66461fdbc90eb
xiongwilee <xiongwilee@foxmail.com>
2019-01-19 18:53:37 +0800

    upd: 优化代码

--- src/skuSpider.js
+++ src/skuSpider.js
@@ -3,11 +3,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const assert = require('assert');
-const rp = require("request-promise");
-const pg = require("promise-generator");
+const pg = require('promise-generator');
 const json2csv = require('json2csv');
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 
 module.exports = skuSpider;
 
@@ -19,28 +18,34 @@ module.exports = skuSpider;
 function skuSpider(siteConfig) {
   const siteProcess = [];
 
-
+  const fields = siteConfig.fields.map((item)=>{
+    return {
+      label: item.name,
+      value: `skuInfo.${item.key}`,
+      default: 'NULL'
+    };
+  });
 
   return puppeteer.launch()
     .then((browser) => {
 
       siteConfig.urls.forEach((url) => {
         siteProcess.push(() => {
-          console.log(`> 开始获取 ${url} 的页面数据...`)
+          console.log(`> 开始获取 ${url} 的页面数据...`);
           return getSku(browser, url, siteConfig.site);
-        })
+        });
       });
 
       return pg(siteProcess)
         .then((data) => {
           browser.close();
 
-          return genCsvData(data);
+          return genCsvData(fields, data);
         })
         .catch((err) => {
-          console.error(err)
-        })
-    })
+          console.error(err);
+        });
+    });
 
 }
 
@@ -65,29 +70,34 @@ function getSku(browser, url, sites) {
 
   return puppeteerHtml(browser, url, siteInfo)
     .then((data) => {
-      let skuInfo;
+      let skuInfo = {};
 
       if (data) {
-        skuInfo = siteInfo.getSkuContent(url, data)
-        console.log(`>> 成功获取 ${skuInfo.sku_name.value} 商品数据，链接：${url}`);
+        const $ = cheerio.load(data);
+        
+        siteInfo.skuContentArr.forEach(( item ) => { 
+          skuInfo[item.name] = item(url, $);
+        });
+
+        console.log(`>> 成功获取：${url}`);
       } else {
         skuInfo = {};
         console.error(`>> 获取 ${url} 商品数据失败！`);
       }
 
       return {
-        siteInfo: siteInfo,
+        siteInfo,
         skuLink: url,
         skuInfo: skuInfo
-      }
+      };
     }).catch((data) => {
-      console.error(`>> 解析 ${url} 页面中的商品数据失败！`);
+      console.error(`>> 解析 ${url} 页面中的商品数据失败: ${JSON.stringify(data)}`);
 
       return {
         siteInfo: siteInfo,
         skuLink: url,
         skuInfo: {}
-      }
+      };
     });
 }
 
@@ -108,7 +118,7 @@ function puppeteerHtml(browser, url, siteInfo) {
       if (siteInfo.onDetailPageLoaded) siteInfo.onDetailPageLoaded(url, page);
 
       // 设置超时时间
-      page.setDefaultNavigationTimeout(10000)
+      page.setDefaultNavigationTimeout(10000);
 
       // 设置页面请求可被截取
       return page.setRequestInterception(true);
@@ -123,6 +133,8 @@ function puppeteerHtml(browser, url, siteInfo) {
 
       return page.goto(url);
     }).then(() => {
+      return page.screenshot({path:'./test.png',  fullPage: true});
+    }).then(() => {
       return page.content();
     }).then((data) => {
       return page.close()
@@ -132,7 +144,7 @@ function puppeteerHtml(browser, url, siteInfo) {
     })
     .catch((err) => {
       console.log(err);
-      return page.close()
+      return page.close();
     });
 }
 
@@ -141,87 +153,7 @@ function puppeteerHtml(browser, url, siteInfo) {
  * @param  {Object} data 商品的结构化数据
  * @return 
  */
-function genCsvData(skuDataLists) {
-
-  const fields = [{
-    label: '电商',
-    value: 'skuInfo.site.value',
-    default: 'NULL'
-  }, {
-    label: '适龄',
-    value: 'skuInfo.book_age.value',
-    default: 'NULL'
-  }, {
-    label: '出版社',
-    value: 'skuInfo.press.value',
-    default: 'NULL'
-  }, {
-    label: '商品名称',
-    value: 'skuInfo.sku_name.value',
-    default: 'NULL'
-  }, {
-    label: '包装',
-    value: 'skuInfo.package_type.value',
-    default: 'NULL'
-  }, {
-    label: '用纸',
-    value: 'skuInfo.paper_type.value',
-    default: 'NULL'
-  }, {
-    label: '售价',
-    value: 'skuInfo.price.value',
-    default: 'NULL'
-  }, {
-    label: '原价',
-    value: 'skuInfo.price_origin.value',
-    default: 'NULL'
-  }, {
-    label: '评价数',
-    value: 'skuInfo.sales_num.value',
-    default: 'NULL'
-  }, {
-    label: '作者名字',
-    value: 'skuInfo.author_name.value',
-    default: 'NULL'
-  }, {
-    label: '重量',
-    value: 'skuInfo.weight.value',
-    default: 'NULL'
-  }, {
-    label: '页数',
-    value: 'skuInfo.pages_num.value',
-    default: 'NULL'
-  }, {
-    label: '开本规格',
-    value: 'skuInfo.format.value',
-    default: 'NULL'
-  }, {
-    label: '商品链接',
-    value: 'skuLink',
-    default: 'NULL'
-  }, {
-    label: '系列丛书名称',
-    value: 'skuInfo.books_name.value',
-    default: 'NULL'
-  }, {
-    label: '内容介绍',
-    value: 'skuInfo.content_intro.value',
-    default: 'NULL'
-  }, {
-    label: '编辑推荐',
-    value: 'skuInfo.editor_recom.value',
-    default: 'NULL'
-  }, {
-    label: '图书简介',
-    value: 'skuInfo.book_intro.value',
-    default: 'NULL'
-  }, {
-    label: 'ISBN书号',
-    value: 'skuInfo.ISBN.value',
-    default: 'NULL'
-  }];
-
-
+function genCsvData(fields, skuDataLists) {
   try {
     const content = json2csv({ data: skuDataLists, fields: fields });
 
@@ -229,14 +161,14 @@ function genCsvData(skuDataLists) {
     const nowStr = `${now.getFullYear()}${now.getMonth()+1}${now.getMonth()}${now.getHours()}${now.getMinutes()}${now.getSeconds()}`;
     const fileName = path.resolve(`./${nowStr}-${skuDataLists.length}.csv`);
 
-    saveCsv(fileName, content)
+    saveCsv(fileName, content);
 
-    console.log(`> 成功生成 ${fileName} ！`)
+    console.log(`> 成功生成 ${fileName} ！`);
 
     return skuDataLists;
   } catch (err) {
     console.error(err);
-    return
+    return;
   }
 }
 
